a95e475322d8539e190960782c8b35eba6ab6ab3
中平翔太 <nakanakasho.0907@gmail.com>
2023-02-18 10:50:58 +0900

    Add: TOPコンポーネント・ヘッダコンポーネント

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,56 @@
 <template>
-  <header>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <header id="header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#" id="header-logo"><img src="@/assets/img/logo.png" /></a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="Navber">
+          <div class="col-5">
+            <form class="d-flex" role="search">
+            <input
+              type="search"
+              class="form-control me-2"
+              placeholder="検索..."
+              aria-label="検索..."
+            />
+            <button type="submit" class="btn btn-outline-success flex-shrink-0">検索</button>
+          </form>
+          </div>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">ホーム</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">リンク</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                href="#"
+                class="nav-link dropdown-toggle"
+                role="button"
+                data-toggle="dropdown"
+                aria-expanded="false"
+              >
+                ドロップダウン
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">メニュー1</a></li>
+                <li><a class="dropdown-item" href="#">メニュー2</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li><a class="dropdown-item" href="#">その他</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">無効</a>
+            </li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container-fluid -->
     </nav>
   </header>
   <router-view />
@@ -13,22 +61,19 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
-header {
-  nav {
-    padding: 30px;
+#header {
+  border-bottom: 2px solid lightgray;
+
+  &-logo img {
+    width: 150px;
   }
 
-  nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
-
-  nav a.router-link-exact-active {
-    color: #42b983;
+  &-search {
+    width: 400px;
+    height: auto;
   }
 }
 </style>
