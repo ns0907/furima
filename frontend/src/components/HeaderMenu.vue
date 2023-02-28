8a16261609e27887dce2b1452e28dc0038e7bf4d
s.nakahira <nakahira.engineer@gmail.com>
2023-02-28 23:25:13 +0900

    Add: ログインモーダル

--- frontend/src/components/HeaderMenu.vue
+++ frontend/src/components/HeaderMenu.vue
@@ -10,18 +10,20 @@
         <div class="collapse navbar-collapse justify-content-end" id="Navber">
           <div class="col-5">
             <form class="d-flex" role="search">
-            <input
-              type="search"
-              class="form-control me-2"
-              placeholder="検索..."
-              aria-label="検索..."
-            />
-            <button type="submit" class="btn btn-outline-success flex-shrink-0">検索</button>
-          </form>
+              <input
+                type="search"
+                class="form-control me-2"
+                placeholder="検索..."
+                aria-label="検索..."
+              />
+              <button type="submit" class="btn btn-outline-success flex-shrink-0">検索</button>
+            </form>
           </div>
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="#">ログイン</a>
+              <a class="nav-link" href="#" data-toggle="modal" data-target="#loginModal"
+                >ログイン</a
+              >
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">会員登録</a>
@@ -53,15 +55,61 @@
       <!-- /.container-fluid -->
     </nav>
   </header>
+  <div id="overlay" v-show="showContent">
+    <div id="loginContent">
+      <span class="square_btn" @click="closeModal"></span>
+    </div>
+  </div>
+
+  <!-- ボタン・リンククリック後に表示される画面の内容 -->
+  <div
+    class="modal fade"
+    id="loginModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="basicModal"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog" style="max-width: 75%;">
+      <div class="modal-content p-3">
+        <div class="modal-header border-0 mx-auto">
+          <div class="modal-title text-center h3">ログイン</div>
+        </div>
+        <div class="modal-body">
+          <p>メールアドレス：<input type="text" class="form-control" /></p>
+          <p>パスワード：<input type="text" class="form-control" /></p>
+        </div>
+        <div class="modal-footer border-0">
+          <button type="button" class="btn btn-primary w-100" @click="login">ログイン</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: 'HeaderMenuComponent',
+  setup() {
+    // data
+    const showContent = ref(false);
+
+    // methods
+    const openModal = () => {
+      showContent.value = true;
+    };
+    const closeModal = () => {
+      showContent.value = false;
+    };
+
+    // computed
+
+    // lifecycle hooks
+
+    return { showContent, openModal, closeModal };
+  },
+};
 </script>
 
 <style lang="scss">
@@ -75,6 +123,66 @@ export default {
   &-search {
     width: 400px;
     height: auto;
+  }
+}
+
+#overlay {
+  /* 要素を重ねた時の順番 */
+  z-index: 1;
+
+  /* 画面全体を覆う設定 */
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  /* 画面の中央に要素を表示させる設定 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#loginContent {
+  z-index: 2;
+  width: 75%;
+  padding: 3em;
+  background: #fff;
+
+  input {
+    width: 100%;
+  }
+
+  // ×ボタン
+  .square_btn {
+    display: block;
+    position: relative;
+    width: 30px;
+    height: 30px;
+    border: 2px solid #333; /* 枠の調整 */
+    background: #fff; /* ボタンの背景色 */
+    float: right;
+    cursor: pointer;
+  }
+
+  .square_btn::before,
+  .square_btn::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 3px; /* 棒の幅（太さ） */
+    height: 27px; /* 棒の高さ */
+    background: #333; /* バツ印の色 */
+  }
+
+  .square_btn::before {
+    transform: translate(-50%, -50%) rotate(45deg);
+  }
+
+  .square_btn::after {
+    transform: translate(-50%, -50%) rotate(-45deg);
   }
 }
 </style>
