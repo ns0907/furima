518cc7b856ad6816bc938cf90b13d4da654b6f50
s.nakahira <nakahira.engineer@gmail.com>
2023-03-03 00:00:39 +0900

    Update: LoginModalに分離・×ボタン追加

--- frontend/src/components/HeaderMenu.vue
+++ frontend/src/components/HeaderMenu.vue
@@ -55,59 +55,15 @@
       <!-- /.container-fluid -->
     </nav>
   </header>
-  <div id="overlay" v-show="showContent">
-    <div id="loginContent">
-      <span class="square_btn" @click="closeModal"></span>
-    </div>
-  </div>
-
-  <!-- ボタン・リンククリック後に表示される画面の内容 -->
-  <div
-    class="modal fade"
-    id="loginModal"
-    tabindex="-1"
-    role="dialog"
-    aria-labelledby="basicModal"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div class="modal-content p-3">
-        <div class="modal-header border-0 mx-auto">
-          <div class="modal-title text-center h3">ログイン</div>
-        </div>
-        <div class="modal-body">
-          <p>メールアドレス：<input type="text" class="form-control" /></p>
-          <p>パスワード：<input type="text" class="form-control" /></p>
-        </div>
-        <div class="modal-footer border-0">
-          <button type="button" class="btn btn-primary w-100" @click="login">ログイン</button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <LoginModal></LoginModal>
 </template>
 
 <script>
-import { ref } from 'vue';
+import LoginModal from '@/components/LoginModal';
 export default {
   name: 'HeaderMenuComponent',
+  components: {LoginModal},
   setup() {
-    // data
-    const showContent = ref(false);
-
-    // methods
-    const openModal = () => {
-      showContent.value = true;
-    };
-    const closeModal = () => {
-      showContent.value = false;
-    };
-
-    // computed
-
-    // lifecycle hooks
-
-    return { showContent, openModal, closeModal };
   },
 };
 </script>
@@ -124,72 +80,6 @@ export default {
   &-search {
     width: 400px;
     height: auto;
-  }
-}
-
-#overlay {
-  /* 要素を重ねた時の順番 */
-  z-index: 1;
-
-  /* 画面全体を覆う設定 */
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-
-  /* 画面の中央に要素を表示させる設定 */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-#loginContent {
-  z-index: 2;
-  width: 75%;
-  padding: 3em;
-  background: #fff;
-
-  input {
-    width: 100%;
-  }
-
-  // ×ボタン
-  .square_btn {
-    display: block;
-    position: relative;
-    width: 30px;
-    height: 30px;
-    border: 2px solid #333; /* 枠の調整 */
-    background: #fff; /* ボタンの背景色 */
-    float: right;
-    cursor: pointer;
-  }
-
-  .square_btn::before,
-  .square_btn::after {
-    content: '';
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 3px; /* 棒の幅（太さ） */
-    height: 27px; /* 棒の高さ */
-    background: #333; /* バツ印の色 */
-  }
-
-  .square_btn::before {
-    transform: translate(-50%, -50%) rotate(45deg);
-  }
-
-  .square_btn::after {
-    transform: translate(-50%, -50%) rotate(-45deg);
-  }
-
-  @include mq(sm) {
-    .modal-dialog {
-      max-width: 65% !important;
-    }
   }
 }
 </style>
---
+++ frontend/src/components/LoginModal.vue
@@ -0,0 +1,84 @@
+<template>
+  <!-- ボタン・リンククリック後に表示される画面の内容 -->
+  <div
+    class="modal fade"
+    id="loginModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="basicModal"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content p-3">
+        <div class="modal-header border-0">
+          <div class="modal-title text-center h3">ログイン</div>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+        </div>
+        <div class="modal-body">
+          <p>メールアドレス：<input type="text" class="form-control" /></p>
+          <p>パスワード：<input type="text" class="form-control" /></p>
+        </div>
+        <div class="modal-footer border-0">
+          <button type="button" class="btn btn-primary w-100" @click="login">ログイン</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'LoginModalComponent',
+  setup() {
+    // data
+    const showContent = ref(false);
+
+    // methods
+    const openModal = () => {
+      showContent.value = true;
+    };
+    const closeModal = () => {
+      showContent.value = false;
+    };
+
+    // computed
+
+    // lifecycle hooks
+
+    return { showContent, openModal, closeModal };
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/common/scss/_common.scss";
+
+#overlay {
+  /* 要素を重ねた時の順番 */
+  z-index: 1;
+
+  /* 画面全体を覆う設定 */
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  /* 画面の中央に要素を表示させる設定 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#loginModal {
+  @include mq(sm) {
+    .modal-dialog {
+      max-width: 65% !important;
+    }
+  }
+}
+</style>
