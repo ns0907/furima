15e8dcfda49da2a162b2cf8206cad3011914a253
s.nakahira <nakahira.engineer@gmail.com>
2023-03-26 22:31:32 +0900

    Fix: 使用していないresponse削除

--- frontend/src/functions/RegistHelper.js
+++ frontend/src/functions/RegistHelper.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default function () {
   const regist = async (email, password) => {
     let message = '';
-    await axios.get(process.env.VUE_APP_SANCTUM_URL).then(async (response) => {
+    await axios.get(process.env.VUE_APP_SANCTUM_URL).then(async () => {
       await axios
         .post(process.env.VUE_APP_API_URL + '/user/regist', {
           email: email,
